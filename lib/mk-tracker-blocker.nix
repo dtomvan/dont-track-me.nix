9664a973d594ca6900b694288f9c9bb84d2e52d9
Tom van Dijk <18gatenmaker6@gmail.com>
2025-02-22 16:02:29 +0100

    fix: enabling individual trackers doesn't work

--- lib/mk-tracker-blocker.nix
+++ lib/mk-tracker-blocker.nix
@@ -7,7 +7,7 @@
 	cfg = config.dont-track-me;
 in {
 	options.dont-track-me.trackers."${name}".enable = lib.mkEnableOption "block tracking from ${name}";
-	config = lib.mkIf (cfg.enableAll || cfg.trackers.enable) ({
+	config = lib.mkIf (cfg.enableAll || cfg.trackers."${name}".enable) ({
 		home.sessionVariables = lib.mkIf (envKey != false) {
 			"${envKey}" = envVal;
 		};
